acbe3eb7e29a23311f003d1b86f502ec659e63b7
Tahir7044 <tahir7044523604@gmail.com>
2022-01-09 13:25:50 +0530

    it is a reactjs web app

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {CTA,Brand,Navbar} from "./components"
+import {Footer,Header,Blog,Possibility,Features,WhatGPT3} from "./containers"
+import "./app.css"
+function App() {
+    return (
+        <div className='App'>
+            <div className='gradient__bg'>
+                <Navbar/>
+                <Header/>
+            </div>
+            <Brand/>
+            <WhatGPT3/>
+            <Features/>
+            <Possibility/>
+            <CTA/>
+            <Blog/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default App
